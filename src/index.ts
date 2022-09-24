eb0377ef53b1a7fc97b26c112592ddad70dfabeb
Sergey Filkovskyi <filkovskyi@gmail.com>
2022-09-24 16:46:40 +0300

    feature-add-basic-structure: add basic animation on load

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import {
   ViewerApp,
   AssetManagerPlugin,
+  timeout,
+  SSRPlugin,
+  mobileAndTabletCheck,
+  GBufferPlugin,
+  ProgressivePlugin,
   TonemapPlugin,
-  addBasePlugins,
-  TweakpaneUiPlugin,
-  AssetManagerBasicPopupPlugin,
-  CanvasSnipperPlugin,
-  IViewerPlugin,
+  SSAOPlugin,
+  GroundPlugin,
+  FrameFadePlugin,
+  BloomPlugin,
+  TemporalAAPlugin,
+  RandomizedDirectionalLightPlugin,
   AssetImporter,
+  createStyles,
 } from "webgi";
 import "./styles.css";
 import gsap from "gsap";
@@ -27,6 +34,21 @@ async function setupViewer() {
   const camera = viewer.scene.activeCamera;
   const position = camera.position;
   const target = camera.target;
+
+  // Interface Elements
+  const loaderElement = document.querySelector(".loader") as HTMLElement;
+
+  // Add WEBGi plugins
+  await viewer.addPlugin(GBufferPlugin);
+  await viewer.addPlugin(new ProgressivePlugin(32));
+  await viewer.addPlugin(new TonemapPlugin(true));
+  const ssr = await viewer.addPlugin(SSRPlugin);
+  const ssao = await viewer.addPlugin(SSAOPlugin);
+  await viewer.addPlugin(FrameFadePlugin);
+  await viewer.addPlugin(GroundPlugin);
+  const bloom = await viewer.addPlugin(BloomPlugin);
+  await viewer.addPlugin(TemporalAAPlugin);
+  await viewer.addPlugin(RandomizedDirectionalLightPlugin, false);
 
   // WEBGi loader
   const importer = manager.importer as AssetImporter;
@@ -49,30 +71,51 @@ async function setupViewer() {
     console.log("loaded");
   });
 
-  const introAnimation = () => {
-    const introAnimationTimeLine = gsap.timeline();
-    introAnimationTimeLine.to(".loader", {
-      x: "150%",
-      duration: 0.8,
-      ease: "power4.inOut",
-      delay: 1,
-    });
+  viewer.renderer.refreshPipeline();
+
+  // WEBGi load model
+  await manager.addFromPath("./assets/tesla.glb");
+
+  function introAnimation() {
+    const introTL = gsap.timeline();
+    introTL
+      .to(".loader", {
+        x: "150%",
+        duration: 0.8,
+        ease: "power4.inOut",
+        delay: 1,
+      })
+      .fromTo(
+        ".hero--content",
+        { opacity: 0, x: "-50%" },
+        {
+          opacity: 1,
+          x: "0%",
+          ease: "power4.inOut",
+          duration: 1.8,
+          onComplete: setupScrollAnimation,
+        },
+        "-=1"
+      )
+      .fromTo(
+        ".header--container",
+        { opacity: 0, y: "-100%" },
+        { opacity: 1, y: "0%", ease: "power1.inOut", duration: 0.8 },
+        "-=1"
+      );
+  }
+
+  const setupScrollAnimation = () => {
+    document.body.style.overflowY = "scroll";
+    document.body.removeChild(loaderElement);
+
+    const tl = gsap.timeline({ default: { ease: "none" } });
   };
 
   let needsUpdate = true;
   function onUpdate() {
     needsUpdate = true;
   }
-
-  await viewer.addPlugin(AssetManagerBasicPopupPlugin);
-  await addBasePlugins(viewer);
-  await viewer.addPlugin(CanvasSnipperPlugin);
-
-  viewer.renderer.refreshPipeline();
-  await manager.addFromPath("./assets/tesla.glb");
-  const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin);
-
-  uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin);
 }
 
 setupViewer();
