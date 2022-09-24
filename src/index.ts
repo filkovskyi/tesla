d8f2b9c7ffbb35c0c9b38bc04b172bc9332e4e2e
Sergey Filkovskyi <filkovskyi@gmail.com>
2022-09-24 15:43:18 +0300

    feature-add-basic-structure: add gsap and basic animation on loaded

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,78 @@
 import {
-    ViewerApp,
-    AssetManagerPlugin,
-    GBufferPlugin,
-    timeout,
-    ProgressivePlugin,
-    TonemapPlugin,
-    SSRPlugin,
-    SSAOPlugin,
-    DiamondPlugin,
-    FrameFadePlugin,
-    GLTFAnimationPlugin,
-    GroundPlugin,
-    BloomPlugin,
-    TemporalAAPlugin,
-    AnisotropyPlugin,
-    GammaCorrectionPlugin,
-    addBasePlugins,
-    ITexture,
-    TweakpaneUiPlugin,
-    AssetManagerBasicPopupPlugin,
-    CanvasSnipperPlugin,
-    IViewerPlugin,
-
-    // Color, // Import THREE.js internals
-    // Texture, // Import THREE.js internals
+  ViewerApp,
+  AssetManagerPlugin,
+  TonemapPlugin,
+  addBasePlugins,
+  TweakpaneUiPlugin,
+  AssetManagerBasicPopupPlugin,
+  CanvasSnipperPlugin,
+  IViewerPlugin,
+  AssetImporter,
 } from "webgi";
 import "./styles.css";
+import gsap from "gsap";
 
 async function setupViewer() {
-    // Initialize the viewer
-    const viewer = new ViewerApp({
-        canvas: document.getElementById("webgi-canvas") as HTMLCanvasElement,
+  // Initialize the viewer
+  const viewer = new ViewerApp({
+    canvas: document.getElementById("webgi-canvas") as HTMLCanvasElement,
+    useRgbm: true,
+    useGBufferDepth: true,
+    isAntialiased: false,
+  });
+
+  viewer.renderer.displayCanvasScaling = Math.min(window.devicePixelRatio, 1);
+
+  const manager = await viewer.addPlugin(AssetManagerPlugin);
+  const camera = viewer.scene.activeCamera;
+  const position = camera.position;
+  const target = camera.target;
+
+  // WEBGi loader
+  const importer = manager.importer as AssetImporter;
+
+  importer.addEventListener("onStart", (ev) => {
+    target.set(8.16, -0.13, 0.51);
+    position.set(3.6, -0.04, -3.93);
+    onUpdate();
+  });
+
+  importer.addEventListener("onProgress", (ev) => {
+    const progressRatio = ev.loaded / ev.total;
+    document
+      .querySelector(".progress")
+      ?.setAttribute("style", `transform: scaleX(${progressRatio})`);
+  });
+
+  importer.addEventListener("onLoad", (ev) => {
+    introAnimation();
+    console.log("loaded");
+  });
+
+  const introAnimation = () => {
+    const introAnimationTimeLine = gsap.timeline();
+    introAnimationTimeLine.to(".loader", {
+      x: "150%",
+      duration: 0.8,
+      ease: "power4.inOut",
+      delay: 1,
     });
+  };
 
-    // Add some plugins
-    const manager = await viewer.addPlugin(AssetManagerPlugin);
+  let needsUpdate = true;
+  function onUpdate() {
+    needsUpdate = true;
+  }
 
-    // Add a popup(in HTML) with download progress when any asset is downloading.
-    await viewer.addPlugin(AssetManagerBasicPopupPlugin);
+  await viewer.addPlugin(AssetManagerBasicPopupPlugin);
+  await addBasePlugins(viewer);
+  await viewer.addPlugin(CanvasSnipperPlugin);
 
-    // Add plugins individually.
-    // await viewer.addPlugin(GBufferPlugin)
-    // await viewer.addPlugin(new ProgressivePlugin(32))
-    // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
-    // await viewer.addPlugin(GammaCorrectionPlugin)
-    // await viewer.addPlugin(SSRPlugin)
-    // await viewer.addPlugin(SSAOPlugin)
-    // await viewer.addPlugin(DiamondPlugin)
-    // await viewer.addPlugin(FrameFadePlugin)
-    // await viewer.addPlugin(GLTFAnimationPlugin)
-    // await viewer.addPlugin(GroundPlugin)
-    // await viewer.addPlugin(BloomPlugin)
-    // await viewer.addPlugin(TemporalAAPlugin)
-    // await viewer.addPlugin(AnisotropyPlugin)
+  viewer.renderer.refreshPipeline();
+  await manager.addFromPath("./assets/tesla.glb");
+  const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin);
 
-    // or use this to add all main ones at once.
-    await addBasePlugins(viewer);
-
-    // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
-    await viewer.addPlugin(CanvasSnipperPlugin);
-
-    // This must be called once after all plugins are added.
-    viewer.renderer.refreshPipeline();
-
-    //await manager.addFromPath("./assets/classic-watch.glb")
-    await manager.addFromPath("./assets/tesla.glb");
-
-    // Load an environment map if not set in the glb file
-    // await viewer.scene.setEnvironment(
-    //     await manager.importer!.importSinglePath<ITexture>(
-    //         "./assets/environment.hdr"
-    //     )
-    // );
-
-    // Add some UI for tweak and testing.
-    const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin);
-    // Add plugins to the UI to see their settings.
-    uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin);
+  uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin);
 }
 
 setupViewer();
