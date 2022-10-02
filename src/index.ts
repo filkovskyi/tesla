1be3e189476a169d7af8a664aede07a13fedff01
Sergey Filkovskyi <filkovskyi@gmail.com>
2022-10-02 19:48:07 +0300

    feature-add-basic-structure: fix animation for explore button

--- src/index.ts
+++ src/index.ts
@@ -40,13 +40,13 @@ async function setupViewer() {
   const target = camera.target
 
   // Interface Elements
-  // const exploreView = document.querySelector('.cam-view-5') as HTMLElement
-  // const canvasView = document.getElementById('webgi-canvas') as HTMLElement
-  // const canvasContainer = document.getElementById('webgi-canvas-container') as HTMLElement
-  // const exitContainer = document.querySelector('.exit--container') as HTMLElement
+  const exploreView = document.querySelector('.cam-view-5') as HTMLElement
+  const canvasView = document.getElementById('webgi-canvas') as HTMLElement
+  const canvasContainer = document.getElementById('webgi-canvas-container') as HTMLElement
+  const exitContainer = document.querySelector('.exit--container') as HTMLElement
   const loaderElement = document.querySelector('.loader') as HTMLElement
-  // const header = document.querySelector('.header') as HTMLElement
-  // const bodyButton = document.querySelector('.button--body') as HTMLElement
+  const header = document.querySelector('.header') as HTMLElement
+  const bodyButton = document.querySelector('.button--body') as HTMLElement
 
   // Add WEBGi plugins
   await viewer.addPlugin(GBufferPlugin)
@@ -106,8 +106,6 @@ async function setupViewer() {
     const itemObject = viewer.scene.findObjectsByName(obj)[0];
     teslaObjects.push(itemObject)
   }
-  console.log(teslaObjects);
-
 
   window.scrollTo(0, 0)
   await timeout(50)
@@ -126,10 +124,20 @@ async function setupViewer() {
 
     const tl = gsap.timeline();
 
+    function hideDoors() {
+      teslaObjects[0].visible = false
+      teslaObjects[1].visible = false
+    }
+
+    function showDoors() {
+      teslaObjects[0].visible = true
+      teslaObjects[1].visible = true
+    }
+
     tl
       // CAM-2
       .to(position, {
-        x: -5.7326804643, y: 1.6524931014, z: 0.1342477293,
+        x: -3.431713076, y: 0.8075677753, z: -0.1423553514,
         scrollTrigger: {
           trigger: ".cam-view-2",
           start: "top bottom",
@@ -138,10 +146,11 @@ async function setupViewer() {
           immediateRender: false,
           markers: true
         },
-        onUpdate
+        onUpdate,
+        hideDoors
       })
       .to(target, {
-        x: -0.1, y: -0.2271486935, z: 0.5,
+        x: 0.1688648285, y: -0.121886754, z: 0.0934596791,
         scrollTrigger: {
           trigger: ".cam-view-2",
           start: "top bottom",
@@ -176,7 +185,8 @@ async function setupViewer() {
           immediateRender: false,
           markers: true
         },
-        onUpdate
+        onUpdate,
+        showDoors
       })
       .to(target, {
         x: 0.15, y: - 0.23, z: -0.30,
@@ -213,7 +223,8 @@ async function setupViewer() {
           immediateRender: false,
           markers: true
         },
-        onUpdate
+        onUpdate,
+        showDoors
       })
       .to(target, {
         x: 0.15, y: - 0.23, z: 0.30,
@@ -250,7 +261,8 @@ async function setupViewer() {
           immediateRender: false,
           markers: true
         },
-        onUpdate
+        onUpdate,
+        showDoors
       })
       .to(target, {
         x: -0.25, y: -0.22, z: -0.14,
@@ -302,7 +314,8 @@ async function setupViewer() {
           immediateRender: false,
           markers: true
         },
-        onUpdate
+        onUpdate,
+        showDoors
       })
       .to(target, {
         x: -0.143696334, y: 0.0157212266, z: -0.3114864004,
@@ -328,6 +341,65 @@ async function setupViewer() {
       needsUpdate = false;
     }
   })
+
+  // EXPLORE ALL FEATURES EVENT
+  document.querySelectorAll('.button-explore').forEach(
+    function (currentValue, currentIndex, listObj) {
+      currentValue?.addEventListener('click', () => {
+        exploreView.style.pointerEvents = "none"
+        canvasView.style.pointerEvents = "all"
+        canvasContainer.style.zIndex = "1"
+        header.style.position = "fixed"
+        document.body.style.overflowY = "hidden"
+        document.body.style.cursor = "grab"
+        exploreAnimation()
+      })
+    },
+  );
+
+  function exploreAnimation() {
+    const tlExplore = gsap.timeline()
+
+    tlExplore
+      .to(position,
+        {
+          x: 5, y: 0.3, z: -4.5,
+          duration: 2.5,
+          onUpdate
+        })
+      .to(target, { x: -0.26, y: -0.2, z: 0.9, duration: 2.5, onUpdate }, '-=2.5')
+      .fromTo('.header', { opacity: 0 }, { opacity: 1, duration: 1.5, ease: "power4.out" }, '-=2.5')
+      .to('.section', { opacity: 0, duration: 1.5, ease: "power4.out", }, '-=2.5')
+      .to('.explore--content', { opacity: 0, x: '130%', duration: 1.5, ease: "power4.out", onComplete: onCompleteExplore }, '-=2.5')
+  }
+
+  function onCompleteExplore() {
+    exitContainer.style.display = "flex"
+    if (camera.controls) camera.controls.enabled = true
+  }
+
+  document.querySelector('.button--exit')?.addEventListener('click', () => {
+    exploreView.style.pointerEvents = "all"
+    canvasView.style.pointerEvents = "none"
+    canvasContainer.style.zIndex = "unset"
+    document.body.style.overflowY = "auto"
+    exitContainer.style.display = "none"
+    header.style.position = "absolute"
+    document.body.style.cursor = "default"
+    exitAnimation()
+  })
+
+  // EXIT EVENT
+  function exitAnimation() {
+    if (camera.controls) camera.controls.enabled = false
+
+    const tlExit = gsap.timeline()
+
+    tlExit.to(position, { x: -4.49, y: 1.42, z: -4.05, duration: 1.2, ease: "power4.out", onUpdate })
+      .to(target, { x: 0.15, y: -0.23, z: -0.30, duration: 1.2, ease: "power4.out", onUpdate }, '-=1.2')
+      .to('.explore--content', { opacity: 1, x: '0%', duration: 0.5, ease: "power4.out" }, '-=1.2')
+      .to('.section', { opacity: 1, duration: 1.5, ease: "power4.out" }, '-=2.5')
+  }
 }
 
 setupViewer();
